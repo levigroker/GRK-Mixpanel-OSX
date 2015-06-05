84b9c5468ed527b535abb2af998df2d5a96a92eb
Levi Brown <levigroker@gmail.com>
2015-06-05 12:29:56 -0600

    Create Mac Library project

---
+++ Mixpanel-OSX-Library/Mixpanel-OSX-Library/Mixpanel_OSX_Library.h
@@ -0,0 +1,13 @@
+//
+//  Mixpanel_OSX_Library.h
+//  Mixpanel-OSX-Library
+//
+//  Created by Levi Brown on 6/5/15.
+//  Copyright (c) 2015 Levi Brown. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Mixpanel_OSX_Library : NSObject
+
+@end
---
+++ Mixpanel-OSX-Library/Mixpanel-OSX-Library/Mixpanel_OSX_Library.m
@@ -0,0 +1,13 @@
+//
+//  Mixpanel_OSX_Library.m
+//  Mixpanel-OSX-Library
+//
+//  Created by Levi Brown on 6/5/15.
+//  Copyright (c) 2015 Levi Brown. All rights reserved.
+//
+
+#import "Mixpanel_OSX_Library.h"
+
+@implementation Mixpanel_OSX_Library
+
+@end
